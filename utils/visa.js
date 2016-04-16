adff0705da6e42eaa30a66a873c9a016adfa53a4
Alastair Paragas <alastairparagas@gmail.com>
2016-04-16 12:22:22 -0400

    Working Visa lib

--- utils/visa.js
+++ utils/visa.js
@@ -9,53 +9,50 @@ var request = require('request'),
 * @returns {Promise} Resolve on success, Rejected otherwise
 */
 function visa(apiPath, userDetails, payload) {
-    
-    function promiseExecutor(resolve, reject) {
-        fsPromise
-            .readFile(`${__dirname}/../keys/visa_private.pem`)
-            .then((privateContents) => {
-                return fsPromise
-                    .readFile(`${__dirname}/../keys/visa_certificate.pem`)
-                    .then((certificateContents) => {
-                        return {
-                            privateContents,
-                            certificateContents
-                        };
-                    });
-            })
-            .then(({privateContents, certificateContents}) => {
-                function promiseExecutor(resolve, reject) {
-                    var userId = userDetails.userid,
-                        userPass = userDetails.password;
-                    
-                    request.post({
-                        url : `https://sandbox.api.visa.com/${apiPath}`,
-                        key: privateContents,
-                        cert: certificateContents,
-                        headers: {
-                            'Content-Type' : 'application/json',
-                            'Accept' : 'application/json',
-                            'Authorization' : 'Basic ' + 
-                                (new Buffer(`${userId}:${userPass}`))
-                                .toString('base64')
-                        },
-                        body: payload
-                    }, (err, httpResponse, body) => {
-                        if (err) {
-                            reject(err);
-                        }
-                        resolve({
-                            httpResponse,
-                            body
-                        });
-                    });
-                }
-                return new Promise(promiseExecutor);
+
+    return fsPromise
+        .readFile(`${__dirname}/../keys/visa_private.pem`)
+        .then((privateContents) => {
+        return fsPromise
+            .readFile(`${__dirname}/../keys/visa_certificate.pem`)
+            .then((certificateContents) => {
+                return {
+                    privateContents,
+                    certificateContents
+                };
             });
-        
-    }
-    
-    return new Promise(promiseExecutor);
+        })
+        .then((contents) => {
+            function promiseExecutor(resolve, reject) {
+                var userId = userDetails.userid,
+                    userPass = userDetails.password, 
+                    privateContents = contents.privateContents,
+                    certificateContents = contents.certificateContents;
+
+                request.post({
+                    url : `https://sandbox.api.visa.com/${apiPath}`,
+                    key: privateContents,
+                    cert: certificateContents,
+                    headers: {
+                        'Content-Type' : 'application/json',
+                        'Accept' : 'application/json',
+                        'Authorization' : 'Basic ' + 
+                        (new Buffer(`${userId}:${userPass}`))
+                        .toString('base64')
+                    },
+                    body: JSON.stringify(payload)
+                }, (err, httpResponse, body) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    resolve({
+                        httpResponse,
+                        body
+                    });
+                });
+            }
+            return new Promise(promiseExecutor);
+        });
 }
 
 module.exports = exports = visa;
